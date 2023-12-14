2580862a59ddc42c3075b4205cb7b3a231d8c2ca
estebanspitale <esteban_spitalehotmail.com>
2023-12-13 22:36:29 -0300

    rentals06

--- scripts/rentals.js
+++ scripts/rentals.js
@@ -1,89 +1,39 @@
-const requestURL = 'https://estebanspitale.github.io/finalProjectWdd230/data/rentals.json';
+const pricesURL = "https://estebanspitale.github.io/finalProjectWdd230/data/rentals.json";
 
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
+async function fetchPrices() {
+    try {
+        const response = await fetch(pricesURL);
+        const prices = await response.json();
+        return prices.maxRentalPricing;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // Re-throw the error to be caught by the caller
+    }
+}
 
-    })
-    .then(function (jsonObject) {
-      
-        const rentals = jsonObject['rentals'];
-        let table = document.createElement('table');
-        let thead = document.createElement('thead');
-        let th = document.createElement('th');
-        let tr = document.createElement('tr');
-        let tr2 = document.createElement('tr');
-        let reservation = document.createElement('th');
-        let walkin = document.createElement('th');
-        let rentaltype = document.createElement('th');
-        let maxpersons = document.createElement('th');
-        let halfday = document.createElement('th');
-        let fullday = document.createElement('th');
-        let halfday2 = document.createElement('th');
-        let fullday2 = document.createElement('th');
-
-        
-        reservation.textContent = "Reservation";
-        walkin.textContent = "Walk-In";
-        rentaltype.textContent = "Rental Type";
-        maxpersons.textContent = "Max. Persons";
-        halfday.textContent = "Half Day (3hrs)";
-        fullday.textContent = "Full Day";
-        halfday2.textContent = "Half Day (3hrs)";
-        fullday2.textContent = "Full Day";
-
-       
-        th.setAttribute("colspan", "2");
-        reservation.setAttribute("colspan", "2");
-        walkin.setAttribute("colspan", "2");
-
-
-        table.appendChild(thead);
-        thead.appendChild(tr);
-        tr.appendChild(th);
-        tr.appendChild(reservation);
-        tr.appendChild(walkin);
-        thead.appendChild(tr2);
-        tr2.appendChild(rentaltype);
-        tr2.appendChild(maxpersons);
-        tr2.appendChild(halfday);
-        tr2.appendChild(fullday);
-        tr2.appendChild(halfday2);
-        tr2.appendChild(fullday2);
-
-        
-
-
-        for (let i = 0; i < rentals.length; i++) {
-
-            let tr = document.createElement('tr');
-            let vehicle = document.createElement('td');
-            let persons = document.createElement('td');
-            let reserveHalf = document.createElement('td');
-            let reserveFull = document.createElement('td');
-            let walkHalf = document.createElement('td');
-            let walkFull = document.createElement('td');
-
-
-
-            vehicle.textContent = rentals[i].vehicle;
-            persons.textContent = rentals[i].persons;
-            reserveHalf.textContent = rentals[i].reserveHalf;
-            reserveFull.textContent = rentals[i].reserveFull;
-            walkHalf.textContent = rentals[i].walkHalf;
-            walkFull.textContent = rentals[i].walkFull;
-
-
-
-            table.appendChild(vehicle);
-            table.appendChild(persons);
-            table.appendChild(reserveHalf);
-            table.appendChild(reserveFull);
-            table.appendChild(walkHalf);
-            table.appendChild(walkFull);
-            table.appendChild(tr);
-
+async function displayPrices() {
+    try {
+        const rentalPrices = await fetchPrices();
+        let p = document.querySelector('.price-table');
+        let out = '';
+        for (let price of rentalPrices) {
+            out += `
+            <tr>
+                <td><img src='${price.image}'></td>
+                <td>${price.rentalType}</td>
+                <td>${price.maxPerson}</td>
+                <td>${price.reservationHalf}</td>
+                <td>${price.reservationFull}</td>
+                <td>${price.walkinHalf}</td>
+                <td>${price.walkinFull}</td>
+            </tr>
+            `;
         }
+        p.innerHTML = out;
+    } catch (error) {
+        console.error('Error displaying prices:', error);
+    }
+}
 
-        document.querySelector('div.table').appendChild(table);
-    });
+// Call the async function
+displayPrices();
